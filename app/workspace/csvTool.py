c1125d1cc0d1586d59aa5fb4709e0f545ad5e1fd
Juan Carlos Moreno Sanchez <carlos.moreno.phd@gmail.com>
2023-10-20 22:11:23 -0600

    Fix comma on file

--- app/workspace/csvTool.py
+++ app/workspace/csvTool.py
@@ -2,37 +2,55 @@ from enum import Enum
 from os import path, remove, rename
 
 
-def fix_csv_remove_comma(folder: str, target_csv: str, fixed_file: str = "fixed.csv", overwrite: bool = True, commodin: str = "\t"):
+def fix_csv_remove_commodim(folder: str, target_csv: str, fixed_file: str = "fixed.csv", overwrite: bool = True, commodin: str = "\t"):
     source_file = path.join(folder, target_csv)
     target_file = path.join(folder, fixed_file)
     if path.exists(target_file):
         remove(target_file)
     count = 0
-    with open(target_file, 'wb') as target_csv, open(source_file, 'r') as source_csv:
+    data_fixed = []
+    with open(source_file, 'r') as source_csv:
         lines = source_csv.readlines()
         for line in lines:
             count += 1
-            print(count)
             if count == 1:
-                target_csv.write(bytes(line, 'utf-8'))
+                data_fixed.append(line)
             elif count >= 2:
                 array_line = line.split(commodin)
-                if len(array_line) > 15:
-                    if validate_type(array_line[5], Type_Validate.INT):
-                        target_csv.write(bytes(line, 'utf-8'))
-                    else:
-                        # Find the agronomic year
-                        index_start = 2
-                        start_index = line.find(
-                            array_line[index_start]) + len(array_line[index_start]) + 1
-                        index_end = find_agronomic_year(
-                            array_line=array_line, start_position=3)
-                        if index_end != -1:
-                            end_index = line.find(
-                                array_line[index_end-1]) - 1
-                            newLine = line.replace(
-                                line[start_index:end_index], line[start_index:end_index].replace(commodin, ""))
-                            target_csv.write(bytes(newLine, 'utf-8'))
+                if count > 12590 and count < 12890:
+                    print(line)
+                if validate_type(array_line[5], Type_Validate.INT):
+                    data_fixed.append(line)
+                else:
+                    index_start = 2
+                    start_index = line.find(
+                        array_line[index_start]) + len(array_line[index_start]) + 1
+                    index_end = find_agronomic_year(
+                        array_line=array_line, start_position=3)
+                    if index_end != -1:
+                        end_index = line.find(
+                            array_line[index_end-1]) - 1
+                        newLine = line.replace(
+                            line[start_index:end_index], line[start_index:end_index].replace(commodin, ""))
+                        data_fixed.append(newLine)
+    with open(target_file, 'wb') as target_csv:
+        for data in data_fixed:
+            target_csv.write(bytes(data, 'utf-8'))
+    if overwrite:
+        if path.exists(source_file):
+            remove(source_file)
+        rename(src=target_file, dst=source_file)
+
+
+def fix_csv_remove_comma(folder: str, target_csv: str, fixed_file: str = "fixed.csv", overwrite: bool = True):
+    source_file = path.join(folder, target_csv)
+    target_file = path.join(folder, fixed_file)
+    if path.exists(target_file):
+        remove(target_file)
+    with open(source_file, 'r') as source_csv, open(target_file, 'wb') as target_csv:
+        lines = source_csv.readlines()
+        for line in lines:
+            target_csv.write(bytes(line.replace(",",""), 'utf-8'))
     if overwrite:
         if path.exists(source_file):
             remove(source_file)
@@ -61,4 +79,4 @@ class Type_Validate(Enum):
 
 if __name__ == "__main__":
     fix_csv_remove_comma(folder="/home/yeiden/Documents/wirk",
-                         target_csv="test.csv", overwrite=False)
+                         target_csv="raw.csv", overwrite=False)
