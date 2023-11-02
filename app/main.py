ad29cbda676188bcd713bf75db46e89624dcb89b
Juan Carlos Moreno Sanchez <carlos.moreno.phd@gmail.com>
2023-11-02 12:48:45 -0600

    Adding new env and infinity loop

--- app/main.py
+++ app/main.py
@@ -1,12 +1,21 @@
 from workspace.toWork import WorkSpace
+from os import getenv
+from dotenv import load_dotenv
+import time
+
+load_dotenv()
 
 
 def run_project():
-    work_space = WorkSpace("/home/yeiden/Documents/wirk/")
-    work_space.work_with_all_zips()
-    # work_space.clean_workspace()
-    # work_space.prepare_folder_files("dataverse_files.zip")
-    # work_space.storage_on_database()
+    while True:
+        print("Working ----- ")
+        try:
+            work_space = WorkSpace(path=getenv(
+                "PATH_TO_SCRAPPING"), api_storage=getenv("URL_DATA_WAREHOUSE"))
+            work_space.work_with_all_zips()
+        except:
+            print("File to work with file")
+        time.sleep(60)
 
 
 if __name__ == "__main__":
